edd162453b3a5d30ba24b4051067fd82b124c697
ahgharib <ahgharib1643@gmail.com>
2025-11-14 22:24:06 +0200

    Local Chat Box with threading

--- Network_client_local.py
+++ Network_client_local.py
@@ -1,11 +1,41 @@
 import socket
+import threading
+
+nickname = input("Enter your nickname: ")
 
 HOST = '192.168.100.22' # server IP (if only then the public IP address with myip.is)
-PORT = 9999 # must be same port
+PORT = 9998 # must be same port
+CODE = 'ascii' # 'utf-8'   Must be the same as the server/ message encode decode
 
-socket =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-socket.connect((HOST, PORT))
+client.connect((HOST, PORT))
 
-socket.send("Hello Server this is Me!".encode('utf-8'))
-print(socket.recv(1024).decode('utf-8'))
+def wait_and_recive():
+    while True:
+        try:
+            msg = client.recv(1024).decode(CODE)
+            if msg == 'NICK':
+                client.send(nickname.encode(CODE))
+            else:
+                print(msg)
+        except:
+            print("An Unexpected Error has Occured please try to login again!")
+            client.close()
+            break
+
+
+def write_and_send():
+    while True:
+        msg = f'{nickname}: {input("")}'
+        # if msg == f'{nickname}: Quit^-^':
+        #     client.send(msg.encode(CODE))
+        #     print("BYE!")
+        #     client.close()
+        #     break
+        client.send(msg.encode(CODE))
+
+recive_thread = threading.Thread(target=wait_and_recive)
+recive_thread.start()
+send_thread = threading.Thread(target=write_and_send)
+send_thread.start()
--- Network_server_local.py
+++ Network_server_local.py
@@ -1,16 +1,69 @@
 import socket
-HOST = '192.168.100.22'
-PORT = 9999
+import threading
 
+HOST = '192.168.100.22' # 0.0.0.0 for the private IP address online
+PORT = 9998
+
+CODE = 'ascii' # 'utf-8'   Must be the same as the client/ message encode decode
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
 
-server.listen(5) # I can put a number for the number of players per time
+server.listen() # I can put a number for the number of players per time
 
-while True:
-    communication_socket, address = server.accept()
-    print(f"Connected To {address}")
-    message = communication_socket.recv(1024).decode('utf-8')
-    print(f"THE MESSAGE: {message}")
-    communication_socket.send(f"GOT Your Message!".encode('utf-8'))
-    print(f"Communication with {address} Ended!")
+clients = []
+nicknames = []
+
+def broadcast(msg):
+    for client in clients:
+        client.send(msg)
+
+
+def delete_client(client, clients, nicknames):
+    index = clients.index(client)
+    clients.remove(client)
+    client.close()
+    nickname = nicknames[index]
+    msg = f"{nickname} Logged out".encode(CODE)
+    broadcast(msg)
+    nicknames.remove(nickname)
+    return clients, nicknames
+            
+
+def handle(client):
+    while True:
+        try:
+            msg = client.recv(1024)
+            broadcast(msg)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            msg = f"{nickname} Logged out".encode(CODE)
+            broadcast(msg)
+            nicknames.remove(nickname)
+            break
+
+            
+def recive():
+    while True:
+        client, address = server.accept()
+        print(f"Connected To {str(address)}")
+        client.send('NICK'.encode(CODE))
+        nickname = client.recv(1024).decode(CODE)
+        nicknames.append(nickname)
+        clients.append(client)
+        print(f"Successfully recived the Client nickname: {nickname}")
+        msg = f"{nickname} logged in".encode(CODE)
+        # if msg == f'{nickname}: Quit^-^':
+        #     client.close()
+        #     msg = f
+        #     broadcast(msg)
+        broadcast(msg)
+        client.send('you have Logged in Successfully ^-^'.encode(CODE))
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+print("Server is Listining........")
+recive()
